a1dd43da26126380075c5432cbaa8d3465f06c9d
Patricia Boc <patricia.boc@student.upt.ro>
2019-04-03 19:30:33 +0300

    mesaj nou

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Labs2019
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello lume!");
         }
     }
 }
